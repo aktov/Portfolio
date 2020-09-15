e391534f921e71b464a10d1cfaf193bfb692fbe1
aktov <atov@ucsd.edu>
2020-09-14 20:07:38 -0700

    Updates

---
+++ precache-manifest.6663101b4315fbaa12b23d956af0d8fc.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5babfd337ba48c6238bebc1b4fef5065",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "792a5652abd4286ce44e",
+    "url": "/Portfolio/static/css/main.f63248ba.chunk.css"
+  },
+  {
+    "revision": "1d1745d631d492f1a0a4",
+    "url": "/Portfolio/static/js/2.339d0bd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.339d0bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792a5652abd4286ce44e",
+    "url": "/Portfolio/static/js/main.859dd432.chunk.js"
+  },
+  {
+    "revision": "25d735113174e2e503ba",
+    "url": "/Portfolio/static/js/runtime-main.e54d9742.js"
+  },
+  {
+    "revision": "2d123c6052e3d40a1f1c34d877c42b79",
+    "url": "/Portfolio/static/media/background.2d123c60.svg"
+  },
+  {
+    "revision": "34bc1cc44d19e056808e08f353826b70",
+    "url": "/Portfolio/static/media/backgroundMobile.34bc1cc4.png"
+  },
+  {
+    "revision": "a908368ba9f593ec72f7e1ae0d56a449",
+    "url": "/Portfolio/static/media/backgroundNew.a908368b.png"
+  },
+  {
+    "revision": "7ffeda96470e06ff6089e3e8697f0db8",
+    "url": "/Portfolio/static/media/backgroundNewMobile.7ffeda96.png"
+  },
+  {
+    "revision": "69aa4055836ad13571fd16f157ceecbd",
+    "url": "/Portfolio/static/media/bullet.69aa4055.svg"
+  },
+  {
+    "revision": "25be27b944417c2130e3a2d66640c59d",
+    "url": "/Portfolio/static/media/cardImage_inTune.25be27b9.png"
+  },
+  {
+    "revision": "127144e10ec80c5933fd94908090823c",
+    "url": "/Portfolio/static/media/cardImage_parking.127144e1.png"
+  },
+  {
+    "revision": "7345982706e977493480f483d563f22b",
+    "url": "/Portfolio/static/media/cardImage_prepd.73459827.png"
+  },
+  {
+    "revision": "d303690cd570dee08f3f74ac4a562f5b",
+    "url": "/Portfolio/static/media/cardImage_yahoo.d303690c.png"
+  },
+  {
+    "revision": "c1a83000f7d3c831e17ff9122b931af9",
+    "url": "/Portfolio/static/media/close.c1a83000.svg"
+  },
+  {
+    "revision": "2382de07512b7b01891c78ea39e61b7e",
+    "url": "/Portfolio/static/media/figmaLogo_black.2382de07.svg"
+  },
+  {
+    "revision": "e424033e1506f9ef9df9e5c739453104",
+    "url": "/Portfolio/static/media/iconEmail.e424033e.svg"
+  },
+  {
+    "revision": "0445fa9729f1d54464e4364156c34ba0",
+    "url": "/Portfolio/static/media/logoBlack.0445fa97.svg"
+  },
+  {
+    "revision": "e60c145e4f386b52c50364bef8497d89",
+    "url": "/Portfolio/static/media/nav.e60c145e.svg"
+  },
+  {
+    "revision": "a951019991a5d7f0a98e46c968534b90",
+    "url": "/Portfolio/static/media/open.a9510199.svg"
+  },
+  {
+    "revision": "0e8e848669cd338b0b24dd73c738ac60",
+    "url": "/Portfolio/static/media/profilePic.0e8e8486.png"
+  }
+]);
